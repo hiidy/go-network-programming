69a576e670df13f078e11072ca2a4cd6d027849e
Woong <hiidy115@gmail.com>
2026-01-03 00:20:28 +0900

    feat: 에코서버에 응답하는 클라이언트코드 구현

--- client.go
+++ client.go
@@ -1,7 +1,50 @@
 package main
 
-import "net"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
 
 func main() {
-	net.Dial()
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+	log.Println("connected to server")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	serverReader := bufio.NewReader(conn)
+
+	fmt.Println("Enter text to send to the server (type 'exit' to quit):")
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == "exit" {
+			break
+		}
+
+		_, err := conn.Write([]byte(text + "\n"))
+		if err != nil {
+			log.Printf("Failed to write to server: %v", err)
+			return
+		}
+
+		response, err := serverReader.ReadString('\n')
+		if err != nil {
+			log.Printf("Failed to read from server: %v", err)
+			return
+		}
+
+		fmt.Printf("Echo from server: %s", response)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Printf("Error reading from stdin: %v", err)
+	}
+
+	log.Println("Connection closed.")
 }
